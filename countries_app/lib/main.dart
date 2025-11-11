1555c1da712f33591848f56a12aafce6e8b77c91
Habtamu8 <habtamuwoldeamanuel8@gmail.com>
2025-11-11 23:49:42 +0300

    dark mode done

--- countries_app/lib/main.dart
+++ countries_app/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:countries_app/blocs/country_bloc.dart' show CountryBloc;
 import 'package:countries_app/blocs/country_event.dart' show FetchCountries;
 import 'package:countries_app/blocs/favorites_bloc.dart' show FavoritesBloc;
-import 'package:countries_app/repository/country_repository.dart' show CountryRepository;
+import 'package:countries_app/repository/country_repository.dart'
+    show CountryRepository;
 import 'package:countries_app/views/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -10,12 +11,15 @@ void main() {
   runApp(
     MultiBlocProvider(
       providers: [
-        BlocProvider(create: (_) => CountryBloc(CountryRepository())..add(FetchCountries())),
+        BlocProvider(
+          create: (_) =>
+              CountryBloc(CountryRepository())..add(FetchCountries()),
+        ),
         BlocProvider(create: (_) => FavoritesBloc()),
       ],
       child: const MyApp(),
-    ));
-    
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -23,18 +27,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
-    return 
-    
-    MaterialApp(
-      
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Countries App',
-      theme: ThemeData(     
-        useMaterial3: true,
-        colorSchemeSeed: Colors.blue,
-      ),
-      home: HomeScreen()
+      themeMode: ThemeMode.system,
+      // Light Theme
+      theme: ThemeData.light().copyWith(primaryColor: Colors.blue),
+
+      darkTheme: ThemeData.dark().copyWith(primaryColor: const Color.fromARGB(255, 139, 153, 164)),
+      home: HomeScreen(),
     );
   }
 }
